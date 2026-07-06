322a3fd1605f2d567be7b8fd4d774c67b79de52c
YMRwithNoworry <168279861+YMRwithNoworry@users.noreply.github.com>
2026-07-06 09:15:42 +0800

    -（修复shader构造器注入）

--- common/src/main/java/com/lowdragmc/lowdraglib2/core/mixins/shader/ShaderInstanceMixin.java
+++ common/src/main/java/com/lowdragmc/lowdraglib2/core/mixins/shader/ShaderInstanceMixin.java
@@ -7,8 +7,8 @@ import com.lowdragmc.lowdraglib2.client.shader.LDProgramDefineManager;
 import com.mojang.blaze3d.shaders.Program;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.ShaderInstance;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceProvider;
-import net.minecraft.util.GsonHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,14 +29,38 @@ public abstract class ShaderInstanceMixin implements ILDShaderInstance {
                                       VertexFormat vertexFormat,
                                       CallbackInfo ci) throws IOException {
         if (ldlib2$shaderJson != null) {
-            this.onCreateShader(resourceProvider, new net.minecraft.resources.ResourceLocation(shaderLocation), vertexFormat, ldlib2$shaderJson);
+            this.onCreateShader(resourceProvider, new ResourceLocation(shaderLocation), vertexFormat, ldlib2$shaderJson);
+            ldlib2$shaderJson = null;
+        }
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/server/packs/resources/ResourceProvider;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/VertexFormat;)V",
+            require = 0,
+            at = @At("RETURN"))
+    public void ldlib2$onCreateShader(ResourceProvider resourceProvider,
+                                      ResourceLocation shaderLocation,
+                                      VertexFormat vertexFormat,
+                                      CallbackInfo ci) throws IOException {
+        if (ldlib2$shaderJson != null) {
+            this.onCreateShader(resourceProvider, shaderLocation, vertexFormat, ldlib2$shaderJson);
+            ldlib2$shaderJson = null;
         }
     }
 
     @ModifyExpressionValue(method = "<init>(Lnet/minecraft/server/packs/resources/ResourceProvider;Ljava/lang/String;Lcom/mojang/blaze3d/vertex/VertexFormat;)V",
+            require = 0,
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/util/GsonHelper;parse(Ljava/io/Reader;)Lcom/google/gson/JsonObject;"))
     private JsonObject ldlib2$captureShaderJson(JsonObject json) {
+        ldlib2$shaderJson = json;
+        return json;
+    }
+
+    @ModifyExpressionValue(method = "<init>(Lnet/minecraft/server/packs/resources/ResourceProvider;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/VertexFormat;)V",
+            require = 0,
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/util/GsonHelper;parse(Ljava/io/Reader;)Lcom/google/gson/JsonObject;"))
+    private JsonObject ldlib2$captureShaderJsonFromLocation(JsonObject json) {
         ldlib2$shaderJson = json;
         return json;
     }
